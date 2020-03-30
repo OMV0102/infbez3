ef85ce1ab09c90078cc2c69ce5a3096b8897ffca
omv0102 <omv98@outlook.com>
2020-03-30 12:23:13 +0700

    скопировали глобальные переменные

--- infbez3/global.cs
+++ infbez3/global.cs
@@ -21,8 +21,15 @@ namespace infbez3
         public static byte[] Simm_byte_key; // Ключ для Симметричного ШИФРОВАНИЯ
         public static byte[] Simm_byte_iv; // Вектор инициализации для Симметричного ШИФРОВАНИЯ
         public static bool Simm_EncryptOrDecrypt; // Режим либо шифруем либо расшифровываем для Симметричного ШИФРОВАНИЯ
-        public static bool Simm_KeyIV_isEntry; // Введенн ли ключ и  вектор или нет
-        public static string Simm_file_extension = "";  // Расширение считанного файла данных
+        public static bool Simm_KeyIV_isEntry; // Введенн ли ключ и  вектор или нет для Симметричного ШИФРОВАНИЯ
+        public static string Simm_file_extension = "";  // Расширение считанного файла данных для Симметричного ШИФРОВАНИЯ
         //=====================================
+        public static byte[] Asimm_byte_in; // Входной массив байтов для Асимметричного ШИФРОВАНИЯ
+        public static byte[] Asimm_byte_out; // ВЫходной массив байтов после Асимметричного ШИФРОВАНИЯ
+        public static byte[] Asimm_byte_keyPublic; // Ключ Открытый для Асимметричного ШИФРОВАНИЯ
+        public static byte[] Asimm_byte_keyPrivate;  // Ключ Закрытый для Асимметричного ШИФРОВАНИЯ
+        public static bool Asimm_EncryptOrDecrypt; // Режим либо шифруем либо расшифровываем для Асимметричного ШИФРОВАНИЯ
+        public static bool Asimm_KeyIV_isEntry; // Введенн ли ключ и  вектор или нет для Асимметричного ШИФРОВАНИЯ
+        public static string Asimm_file_extension = "";  // Расширение считанного файла данных для Асимметричного ШИФРОВАНИЯ
     }
 }
