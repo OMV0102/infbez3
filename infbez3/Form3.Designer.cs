2606bcb77c851e2394d8881ee2ce6f670021252d
omv0102 <omv98@outlook.com>
2020-03-30 12:58:00 +0700

    добавил 3 форму по подобию 2 формы по ка что

---
+++ infbez3/Form3.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace infbez3
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label_simm_entryKeyIV = new System.Windows.Forms.Label();
+            this.btn_confirm_entry = new System.Windows.Forms.Button();
+            this.txt_key = new System.Windows.Forms.TextBox();
+            this.txt_iv = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_generate_key = new System.Windows.Forms.Button();
+            this.btn_generate_iv = new System.Windows.Forms.Button();
+            this.btn_loadKeyIV = new System.Windows.Forms.Button();
+            this.toolTip_LoadKeyIV = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // label_simm_entryKeyIV
+            // 
+            this.label_simm_entryKeyIV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_simm_entryKeyIV.ForeColor = System.Drawing.Color.Maroon;
+            this.label_simm_entryKeyIV.Location = new System.Drawing.Point(5, 4);
+            this.label_simm_entryKeyIV.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_simm_entryKeyIV.Name = "label_simm_entryKeyIV";
+            this.label_simm_entryKeyIV.Size = new System.Drawing.Size(693, 148);
+            this.label_simm_entryKeyIV.TabIndex = 79;
+            this.label_simm_entryKeyIV.Text = resources.GetString("label_simm_entryKeyIV.Text");
+            // 
+            // btn_confirm_entry
+            // 
+            this.btn_confirm_entry.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_confirm_entry.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Green;
+            this.btn_confirm_entry.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.btn_confirm_entry.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_confirm_entry.Location = new System.Drawing.Point(263, 233);
+            this.btn_confirm_entry.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_confirm_entry.Name = "btn_confirm_entry";
+            this.btn_confirm_entry.Size = new System.Drawing.Size(181, 42);
+            this.btn_confirm_entry.TabIndex = 80;
+            this.btn_confirm_entry.TabStop = false;
+            this.btn_confirm_entry.Text = "Подтвердить";
+            this.btn_confirm_entry.UseVisualStyleBackColor = true;
+            this.btn_confirm_entry.Click += new System.EventHandler(this.btn_confirm_entry_Click);
+            // 
+            // txt_key
+            // 
+            this.txt_key.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txt_key.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txt_key.Location = new System.Drawing.Point(78, 158);
+            this.txt_key.MaxLength = 64;
+            this.txt_key.Name = "txt_key";
+            this.txt_key.Size = new System.Drawing.Size(970, 26);
+            this.txt_key.TabIndex = 81;
+            this.txt_key.TabStop = false;
+            this.txt_key.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_key_KeyPress);
+            // 
+            // txt_iv
+            // 
+            this.txt_iv.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txt_iv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txt_iv.Location = new System.Drawing.Point(246, 197);
+            this.txt_iv.MaxLength = 32;
+            this.txt_iv.Name = "txt_iv";
+            this.txt_iv.Size = new System.Drawing.Size(490, 26);
+            this.txt_iv.TabIndex = 82;
+            this.txt_iv.TabStop = false;
+            this.txt_iv.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_iv_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(24, 160);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 84;
+            this.label1.Text = "Ключ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(24, 200);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 20);
+            this.label2.TabIndex = 85;
+            this.label2.Text = "Вектор инициализации (IV):";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btn_generate_key
+            // 
+            this.btn_generate_key.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_generate_key.BackgroundImage")));
+            this.btn_generate_key.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_generate_key.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_generate_key.Location = new System.Drawing.Point(1054, 153);
+            this.btn_generate_key.Name = "btn_generate_key";
+            this.btn_generate_key.Size = new System.Drawing.Size(35, 35);
+            this.btn_generate_key.TabIndex = 86;
+            this.btn_generate_key.TabStop = false;
+            this.btn_generate_key.UseVisualStyleBackColor = true;
+            this.btn_generate_key.Click += new System.EventHandler(this.btn_generate_key_Click);
+            // 
+            // btn_generate_iv
+            // 
+            this.btn_generate_iv.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_generate_iv.BackgroundImage")));
+            this.btn_generate_iv.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_generate_iv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_generate_iv.Location = new System.Drawing.Point(743, 192);
+            this.btn_generate_iv.Name = "btn_generate_iv";
+            this.btn_generate_iv.Size = new System.Drawing.Size(35, 35);
+            this.btn_generate_iv.TabIndex = 87;
+            this.btn_generate_iv.TabStop = false;
+            this.btn_generate_iv.UseVisualStyleBackColor = true;
+            this.btn_generate_iv.Click += new System.EventHandler(this.btn_generate_iv_Click);
+            // 
+            // btn_loadKeyIV
+            // 
+            this.btn_loadKeyIV.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_loadKeyIV.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Green;
+            this.btn_loadKeyIV.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.btn_loadKeyIV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_loadKeyIV.Location = new System.Drawing.Point(858, 200);
+            this.btn_loadKeyIV.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_loadKeyIV.Name = "btn_loadKeyIV";
+            this.btn_loadKeyIV.Size = new System.Drawing.Size(173, 51);
+            this.btn_loadKeyIV.TabIndex = 88;
+            this.btn_loadKeyIV.TabStop = false;
+            this.btn_loadKeyIV.Text = "Загрузить ключ и IV из файла";
+            this.btn_loadKeyIV.UseVisualStyleBackColor = true;
+            this.btn_loadKeyIV.Click += new System.EventHandler(this.btn_loadKeyIV_Click);
+            // 
+            // toolTip_LoadKeyIV
+            // 
+            this.toolTip_LoadKeyIV.AutoPopDelay = 20000;
+            this.toolTip_LoadKeyIV.InitialDelay = 300;
+            this.toolTip_LoadKeyIV.ReshowDelay = 100;
+            this.toolTip_LoadKeyIV.ShowAlways = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 288);
+            this.Controls.Add(this.btn_loadKeyIV);
+            this.Controls.Add(this.btn_generate_iv);
+            this.Controls.Add(this.btn_generate_key);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_iv);
+            this.Controls.Add(this.txt_key);
+            this.Controls.Add(this.btn_confirm_entry);
+            this.Controls.Add(this.label_simm_entryKeyIV);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ШИФРОВАНИЕ: Ввод ключа (Key) и вектора инициализации (IV)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form3_FormClosing);
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label_simm_entryKeyIV;
+        private System.Windows.Forms.Button btn_confirm_entry;
+        private System.Windows.Forms.TextBox txt_key;
+        private System.Windows.Forms.TextBox txt_iv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_generate_key;
+        private System.Windows.Forms.Button btn_generate_iv;
+        private System.Windows.Forms.Button btn_loadKeyIV;
+        private System.Windows.Forms.ToolTip toolTip_LoadKeyIV;
+    }
+}
---
+++ infbez3/Form3.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using System.Collections;
+using System.Text;
+using System.Drawing;
+
+namespace infbez3
+{
+    public partial class Form3 : Form
+    {
+        public Form3(string Alg)
+        {
+            InitializeComponent();
+            AlgName = Alg;
+        }
+
+        public static AesCng aescng;
+        public static TripleDESCng tripledes;
+        public static string AlgName;
+        public Button form1_btn_simm_entryKeyIV;
+
+
+        // при ЗАГРУЗКЕ ФОРМЫ для ввода ключа и IV
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // Выделили память и установили длину ключей и IV
+            // в зависимости от алгоритма
+            aescng = new AesCng();
+            tripledes = new TripleDESCng();
+            if (AlgName == "AES")
+            {
+                this.txt_key.MaxLength = 64;
+                this.txt_iv.MaxLength = 32;
+
+            }
+
+            if (AlgName == "3DES")
+            {
+                this.txt_key.MaxLength = 48;
+                this.txt_iv.MaxLength = 16;
+            }
+
+            // если раннее были введенны ключи то вывести их на форму
+            if (global.Simm_KeyIV_isEntry)
+            {
+                this.txt_key.Text = alg.ByteArrayTOStringHEX(global.Simm_byte_key);
+                this.txt_iv.Text = alg.ByteArrayTOStringHEX(global.Simm_byte_iv);
+            }
+
+            // Подсказка у кнопки загрузки ключа
+            this.toolTip_LoadKeyIV.ToolTipTitle = this.btn_loadKeyIV.Text;
+            this.toolTip_LoadKeyIV.ToolTipIcon = ToolTipIcon.Info;
+            this.toolTip_LoadKeyIV.SetToolTip(this.btn_loadKeyIV, "В файле должно быть две строки в 16-ричном виде.\n1-ая строка: Ключ длинной 64 знака.\n2-ая строка: Вектор(IV) длиной 32 знакак.");
+
+            // Инструкция сверху формы
+            this.label_simm_entryKeyIV.Text = "> Ключом могут быть только 16-ричные цифры (0-9, A-F).\n";
+            this.label_simm_entryKeyIV.Text += "> Длина ключа должна быть обязательно равна " + txt_key.MaxLength + " знакам!\n\n";
+            this.label_simm_entryKeyIV.Text += "> В векторе могут быть только 16-ричные цифры (0-9, A-F).\n";
+            this.label_simm_entryKeyIV.Text += "> Длина должна быть обязательно равна "+ txt_iv.MaxLength + " знакам!\n";
+            
+
+            if (global.Simm_EncryptOrDecrypt) // если загрузили для ШИФРОВАНИЯ
+            {
+                this.Text = "ШИФРОВАНИЕ: Ввод ключа (Key) и вектора инициализации (IV)";
+                // показать кнопки случайно генерации
+                this.btn_generate_key.Visible = true;
+                this.btn_generate_iv.Visible = true;
+                this.label_simm_entryKeyIV.Text += "\n> Стрелки - случайное заполнение ключа и вектора (IV).";
+            }
+            else  // если загрузили для РАСШИФРОВКИ
+            {
+                this.Text = "РАСШИФРОВКА: Ввод ключа (Key) и вектора инициализации (IV)";
+                this.btn_generate_key.Visible = false;
+                this.btn_generate_iv.Visible = false;
+            }
+        }
+
+        // кнопка ПОДТВЕРДИТЬ
+        private void btn_confirm_entry_Click(object sender, EventArgs e)
+        {
+            if(txt_key.Text.Length == txt_key.MaxLength)
+            {
+                if (txt_iv.Text.Length == txt_iv.MaxLength)
+                {
+                    global.Simm_byte_key = alg.StringHEXToByteArray(txt_key.Text); // Запомнили ключ
+                    global.Simm_byte_iv = alg.StringHEXToByteArray(txt_iv.Text); // Запомнили IV
+                    global.Simm_KeyIV_isEntry = true;
+
+                    form1_btn_simm_entryKeyIV.Text = "Изменить ключ и IV (введенно)"; // Изменили название кнопки на основной форме
+                    form1_btn_simm_entryKeyIV.ForeColor = Color.FromKnownColor(KnownColor.Green); // Цвет изменили
+
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Число символов в IV должно быть " + txt_iv.MaxLength.ToString() + "!\nОтредактируйте IV или сгенерируйте случайно.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Число символов в ключе должно быть " + txt_key.MaxLength.ToString() + "!\nОтредактируйте ключ или сгенерируйте случайно.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        // Генерировать ключ
+        private void btn_generate_key_Click(object sender, EventArgs e)
+        {
+            if(AlgName == "AES")
+            {
+                aescng.GenerateKey();
+                this.txt_key.Text = alg.ByteArrayTOStringHEX(aescng.Key);
+            }
+
+            if (AlgName == "3DES")
+            {
+                tripledes.GenerateKey();
+                this.txt_key.Text = alg.ByteArrayTOStringHEX(tripledes.Key);
+            }
+        }
+
+        // Генерировать вектор инициализации IV
+        private void btn_generate_iv_Click(object sender, EventArgs e)
+        {
+            if (AlgName == "AES")
+            {
+                aescng.GenerateIV();
+                this.txt_iv.Text = alg.ByteArrayTOStringHEX(aescng.IV);
+            }
+
+            if (AlgName == "3DES")
+            {
+                tripledes.GenerateIV();
+                this.txt_iv.Text = alg.ByteArrayTOStringHEX(tripledes.IV);
+            }
+        }
+
+        // Ввод символа в поле ключа (только 16-ричные символы)
+        private void txt_key_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((e.KeyChar >= 48 && e.KeyChar <= 57) || (e.KeyChar >= 65 && e.KeyChar <= 70) || (e.KeyChar >= 97 && e.KeyChar <= 102) || e.KeyChar == 8 || e.KeyChar == 127)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        // Ввод символа в поле iv (только 16-ричные символы)
+        private void txt_iv_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 48 && e.KeyChar <= 57) || (e.KeyChar >= 65 && e.KeyChar <= 70) || (e.KeyChar >= 97 && e.KeyChar <= 102) || e.KeyChar == 8 || e.KeyChar == 127)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        // кнопка ЗАГРУЗИТЬ ключ и IV из файла
+        private void btn_loadKeyIV_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Выбрать файл ..."; // Заголовок окна
+            ofd.InitialDirectory = Application.StartupPath; // Папка проекта
+
+            if (ofd.ShowDialog() == DialogResult.OK) // Если выбрали файл
+            {
+                // читаем байты из файла
+                if (ofd.FileName.Length > 0) // Если путь не нулевой
+                {
+                    if (File.Exists(ofd.FileName) == true) // Если указанный файл существует
+                    {
+                        string temp1 = "";
+                        string temp2 = "";
+                        using (StreamReader sr = new StreamReader(ofd.FileName, Encoding.UTF8))
+                        {
+                            temp1 = sr.ReadLine();
+                            temp2 = sr.ReadLine();
+                            if(temp1 == null || temp2 == null || temp1.Length != txt_key.MaxLength || temp2.Length != txt_iv.MaxLength)
+                            {
+                                MessageBox.Show("Ошибка считывания данных!\nПосмотрите подсказку при наведении на кнопку загрузки.", " Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+
+                            // Выводим в форму считанные данные
+                            this.txt_key.Text = temp1;
+                            this.txt_iv.Text = temp2;
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Файла {" + ofd.FileName + "} не существует!", " Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                else
+                {
+                    this.Enabled = false;
+                    MessageBox.Show("Указан неверный путь!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Enabled = true;
+                    return;
+                }
+            }
+            ofd.Dispose();
+        }
+
+        // При закрытии формы
+        private void Form3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Освободили память от aes и 3des
+            aescng.Dispose();
+            tripledes.Dispose();
+        }
+    }
+}
